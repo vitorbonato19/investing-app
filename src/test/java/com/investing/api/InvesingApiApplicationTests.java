8ded6fa73a01a0a24df070567db58efc009627a6
Vitor <vitorbonatocam@gmail.com>
2025-03-17 22:15:37 -0300

    push project

---
+++ src/test/java/com/investing/api/InvesingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investing.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvesingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
